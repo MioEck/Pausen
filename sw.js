c1446ed7958022f4f2f2bcd440c61abd01901d74
MioEck <macbook33@googlemail.com>
2026-07-18 20:17:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pausenplaner-v4.0';
+const CACHE = 'pausenplaner-v4.2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
